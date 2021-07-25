00d5ec87ae911b788bc4b63d6d61f1b5876b1234
= <>
2021-07-25 15:49:07 +0900

    t

---
+++ server.js
@@ -0,0 +1,25 @@
+// dummy server
+var http = require('http');
+var server = http.createServer();
+
+server.on('request', function(req, res) {
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.end();
+});
+server.listen(80);
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === '!ping') {
+		message.channel.send('Pong.');
+	}
+});
+
+client.login(process.env.DISC_TOKEN);
