137c31b9ba14a282e14f86f3873eee957ef41625
y.hirobe <y.hirobe@tcdigital.jp>
2021-07-27 19:51:49 +0000

    sake

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ server.listen(8080);
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const Sake = require('./sake.js');
+
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -20,6 +23,11 @@ client.on('message', message => {
 	if (message.content === '!ping') {
 		message.channel.send('Pong.');
 	}
+	if(message.content.indexOf("酒")>=0)
+  	{
+    		message.channel.send(Sake.getSakeInfoR());
+  	}
+
 });
 
 client.login(process.env.DISC_TOKEN);
